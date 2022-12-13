8484c434a514d102735225a08eb065085b874d9a
FacuTombesi <facundomtombesi@gmail.com>
2022-12-13 10:33:05 -0300

    01 homework

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -1,6 +1,36 @@
-process.stdout.write("prompt > ");
-process.stdin.on("data", (data) => {
-  // cuando escribo, leer bien lo que escribo
-  // ejecutar una funcion que tenga ese nombre
-  // esas funciones, estarán en commands/index.js
+const commands = require("./commands/index.js")
+
+process.stdout.write("prompt > ") // Inicia una línea para empezar a escribir
+
+process.stdin.on("data", (data) => { // Recibe data + una callback que recibe data como argumento
+  const input = data.toString().trim().split(" ") // Convierte lo escrito pasado por data en string y le quita espacios innecesarios
+  const cmd = input.shift() // Quita la primera palabra y la pasa como comando
+  const args = input.join(" ") // Devuelve el resto de input como string
+  // process.stdout.write("You typed: " + cmd + "\n") // Devuelve lo que escribí
+  
+  // if(cmd === "pwd") commands.pwd()
+  // if(cmd === "date") commands.date()
+
+  /* Switch sirve para pocos métodos pero no es eficiente con muchos */
+  // switch(cmd) { // Si cmd es pwd o data, devuelve pwd o date (importados de index.js)
+  //   case "pwd":
+  //     commands.pwd()
+  //     break
+  //   case "date":
+  //     commands.date()
+  //     break
+  // }
+
+  // if(commands.hasOwnProperty(cmd)) commands[cmd]() // Commands ejecuta los comandos importados de index con lo recibido por cmd
+  // else process.stdout.write("El comando no existe") // Si el comando no existe, devuelve un error
+ 
+  /* El if y el else en una línea. "?" = if, ":" = else */
+  // commands.hasOwnProperty(cmd))?commands[cmd]():process.stdout.write("El comando no existe")
+  /* o... */
+  commands[cmd]
+    ?commands[cmd](args)
+    :process.stdout.write("El comando no existe")
+
+  /* El último \nprompt ya no sirve ya que lo usa en la constante write de index */
+  // process.stdout.write("\nprompt > ") // Crea una nueva línea después de escribir para volver a ingresar comandos
 });
--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,5 +1,54 @@
-
+const fs = require("fs")
+const request = require("request")
+const write = (value) => { // Creo una función que reemplaza a process.stout.write()
+    process.stdout.write(value + "\n")
+    process.stdout.write("prompt > ")
+}
 
 module.exports = {
+    pwd: () => {
+        const dirname = process.cwd().split("\\").at(-1) // Guarda el resultado de process.pwd(), divite el directorio en un array y at(-1) me devuelve el nombre de la última carpeta del directorio
+        write(dirname)
+        // write(process.cwd().split("\\").at(-1)) // Otra alternativa en una línea
+    },
 
+    date: () => {
+        write(Date()) // Devuelve la fecha actual
+    },
+
+    ls: () => {
+        fs.readdir(".", (err, files) => { // err = errores y files = array de nombres de archivo
+            if(err) throw err
+            const text = files.join("\n") // Convierte el array de files en string en nuevas líneas
+            write(text + "\n")
+        })
+    },
+
+    echo: (text) => { // Devuelve texto
+        write(text)
+    },
+
+    cat: (filename) => { // Devuelve todas las líneas del archivo
+        fs.readFile("./" + filename, "utf-8" /* para que no muestre el buffer */, (err, file) => {
+            write(file)
+        })
+    },
+
+    head: (filename) => { // Devuelve las primeras líneas del archivo
+        fs.readFile("./" + filename, "utf-8", (err, file) => {
+            write(file.split("\n").slice(0, 5)/* slice arranca desde el principio hasta la 4° línea (5 - 1) */.join("\n"))
+        })
+    },
+
+    tail: (filename) => { // Devuelve las últimas líneas del archivo
+        fs.readFile("./" + filename, "utf-8", (err, file) => {
+            write(file.split("\n").slice(-5).join("\n"))
+        })
+    },
+
+    curl: (url) => { // Devuelve el código de la url pasada por argumento
+        request(url, (err, res, body) => {
+            write(body)
+        })
+    }
 }
