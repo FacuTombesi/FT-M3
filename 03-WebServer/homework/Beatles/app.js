277ffb7e3f5f8d2b1d7a9ce6859ebbadb4c77ceb
FacuTombesi <facundomtombesi@gmail.com>
2023-01-03 10:44:00 -0300

    03 homework

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -22,3 +22,64 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+http
+  .createServer((req, res) => {
+    const { url } = req
+    const splited = url.split("/").slice(1) // Divide la url por las "/" y después quita la primera que está vacía
+
+    /* Método con if */
+    /* if (splited[0] === "api") { // Chequeo si tengo api
+      apiController(splited[1], res) // Llamo a apiController
+    } else {
+      viewsController(splited[0], res) // Si no hay api, llamo a viewsController y devuelve el nombre que recibe
+    } */
+
+    /* Método con if simplificado */
+    splited[0] === "api" // if
+      ? /* si se cumple... */ apiController(splited[1], res) 
+      : /* si no se cumple... */ viewsController(splited[0], res)
+  })
+  .listen(3001, "localhost")
+
+const apiController = (name, res) => {
+  if (!name) { // Si no tengo name
+    // res.writeHead(200, { "Content-Type": "application/json" }) // Agarra todo el archivo
+    // res.end(JSON.stringify(beatles)) // Y lo devuelve como un JSON
+    sendJson(200, beatles, res)
+  } else {
+    const fullName = name.replace("%20", " ") // Reemplaza el "%20" que devuelve la consola para interpretar espacios
+    const beatle = beatles.find((b) => b.name == fullName) // Busca el Beatle con el nombre que recibe
+
+    /*
+    beatle // Chequea el Beatle pedido
+      ? res.writeHead(200, { "Content-Type": "application/json" }).end(JSON.stringify(beatle)) // Si existe, lo devuelve
+      : res.writeHead(404, { "Content-Type": "application/json" }).end(JSON.stringify({ error: "Beatle not found" })) // Si noexiste, devuelve un error
+    */
+
+    beatle
+      ? sendJson(200, beatle, res)
+      : sendJson(404, { error: "Beatle not found" }, res)
+    
+  }
+}
+
+const viewsController = (name, res) => {
+  if (!name) { // Si no hay name
+    const html = fs.readFileSync("./index.html") // Lee y guarda el archivo index
+    return res.writeHead(200, { "Content-Type": "text/html" }).end(html) // Devuelve el html
+  } else {
+    const fullName = name.replace("%20", " ")
+    const beatle = beatles.find((b) => b.name == fullName)
+    let html = fs.readFileSync("./beatle.html", "utf-8") // Busca y lee a cada Beatle y reemplaza la info dependiendo de quién reciba
+    html = html.replaceAll("{name}", beatle.name)
+    html = html.replace("{birthdate}", beatle.birthdate)
+    html = html.replace("{profilePic}", beatle.profilePic)
+    res.writeHead(200, { "Content-Type": "text/html" }).end(html)
+  }
+}
+
+const sendJson = (status, content, res) => { // Reemplaza al res.writeHead para cada caso
+  res.writeHead(status, { "Content-Type": "application/json" })
+  res.end(JSON.stringify(content))
+}
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,32 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+http
+    .createServer((req, res) => { // Crea un servidor con un requirement y una response
+        const { url } = req
+
+        /* Método con switch (sirve para pocos casos) */
+        /* switch (url) {
+            case "/arcoiris":
+                const img = fs.readFileSync(__dirname + "/images/arcoiris_doge.jpg")
+                res.writeHead(200, { "Content-Type": "image/jpeg" })
+                res.end(img)
+                break;
+            // ... más casos
+            default:
+                break;
+        } */
+
+        /* Método con try */
+        const name = url.slice(1) // Agarra el /name sin el "/"
+
+        try {
+            const img = fs.readFileSync(__dirname + `/images/${name}_doge.jpg`) // Guarda en una variable lo que haya en la dirección con el nombre pedido
+            res.writeHead(200, { "Content-Type": "image/jpeg" }) // La respuesta recibe el tipo de contenido que hay en la dirección
+            res.end(img) // Devuelve la imagen
+        }   catch (error) {
+            res.writeHead(404, { "Content-Type": "text/plain" }) // En caso que no exista, devuelve un texto
+            res.end(`No existe el doge ${name}`)
+        }
+    })
+    .listen(3001, "127.0.0.1") // Defino el puerto donde se va a montar
