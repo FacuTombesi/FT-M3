721fdf58317ce28649e03ccba7cc254b63821149
FacuTombesi <facundomtombesi@gmail.com>
2023-01-05 10:32:18 -0300

    07 homework

--- 07-AsyncAwait/AsyncAwait/homework/exercise-two.js
+++ 07-AsyncAwait/AsyncAwait/homework/exercise-two.js
@@ -25,7 +25,7 @@ args.forEach(function(arg){
   if (problem) problem();
 });
 
-function problemA () {
+async function problemA () {
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
    *
    * A. loggea el poema dos stanza uno y stanza dos en cualquier orden
@@ -36,24 +36,30 @@ function problemA () {
    */
 
   // callback version
-  async.each(['poem-two/stanza-01.txt', 'poem-two/stanza-02.txt'],
-    function (filename, eachDone) {
-      readFile(filename, function (err, stanza) {
-        console.log('-- A. callback version --');
-        blue(stanza);
-        eachDone();
-      });
-    },
-    function (err) {
-      console.log('-- A. callback version done --');
-    }
-  );
+  // async.each(['poem-two/stanza-01.txt', 'poem-two/stanza-02.txt'],
+  //   function (filename, eachDone) {
+  //     readFile(filename, function (err, stanza) {
+  //       console.log('-- A. callback version --');
+  //       blue(stanza);
+  //       eachDone();
+  //     });
+  //   },
+  //   function (err) {
+  //     console.log('-- A. callback version done --');
+  //   }
+  // );
 
   // AsyncAwait version
-
+  const stanzas = await Promise.all([ // Promise.all() lee varias promesas al mismo tiempo y devuelve una promesa
+    promisifiedReadFile('poem-two/stanza-01.txt'),
+    promisifiedReadFile('poem-two/stanza-02.txt')
+  ])
+  blue(stanzas[0])
+  blue(stanzas[1])
+  console.log('done')
 }
 
-function problemB () {
+async function problemB () {
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
    *
    * B. loggea todas las stanzas en poema dos, en cualquier orden y loggea
@@ -68,24 +74,36 @@ function problemB () {
   });
 
   // callback version
-  async.each(filenames,
-    function (filename, eachDone) {
-      readFile(filename, function (err, stanza) {
-        console.log('-- B. callback version --');
-        blue(stanza);
-        eachDone();
-      });
-    },
-    function (err) {
-      console.log('-- B. callback version done --');
-    }
-  );
+  // async.each(filenames,
+  //   function (filename, eachDone) {
+  //     readFile(filename, function (err, stanza) {
+  //       console.log('-- B. callback version --');
+  //       blue(stanza);
+  //       eachDone();
+  //     });
+  //   },
+  //   function (err) {
+  //     console.log('-- B. callback version done --');
+  //   }
+  // );
 
   // AsyncAwait version
+  const promises = filenames.map((file) => promisifiedReadFile(file)) // Mapea filenames y devuelve una promesa por cada archivo y los guarda en un array promises
 
+  /*
+  [
+    'poem-two/stanza-01.txt',   --->    promisifiedReadFile('poem-two/stanza-01.txt')
+    'poem-two/stanza-02.txt',   --->    promisifiedReadFile('poem-two/stanza-02.txt')
+    'poem-two/stanza-03.txt'    --->    promisifiedReadFile('poem-two/stanza-03.txt')
+  ]
+  */
+
+  const stanzas = await Promise.all(promises) // Hace el Promise.all() del array promises
+  stanzas.forEach((stanza) => blue(stanza)) // forEach() realiza el blue() de cada una de las stanzas
+  console.log('done')
 }
 
-function problemC () {
+async function problemC () {
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
    *
    * C. Lee y loggea todas las stanzas en el poema dos, *en orden* y
@@ -101,24 +119,36 @@ function problemC () {
   });
 
   // callback version
-  async.eachSeries(filenames,
-    function (filename, eachDone) {
-      readFile(filename, function (err, stanza) {
-        console.log('-- C. callback version --');
-        blue(stanza);
-        eachDone();
-      });
-    },
-    function (err) {
-      console.log('-- C. callback version done --');
-    }
-  );
+  // async.eachSeries(filenames,
+  //   function (filename, eachDone) {
+  //     readFile(filename, function (err, stanza) {
+  //       console.log('-- C. callback version --');
+  //       blue(stanza);
+  //       eachDone();
+  //     });
+  //   },
+  //   function (err) {
+  //     console.log('-- C. callback version done --');
+  //   }
+  // );
 
   // AsyncAwait version
+  const promises = filenames.map((file) => promisifiedReadFile(file))
 
+  // promises.forEach(async (promise) => { // Como esta es una función dentro de otra función, el async va en la segunda
+  //   const stanza = await promise
+  //   blue(stanza)
+  // })
+
+  for (const promise of promises) { // for (.. of ..) las itera en orden
+    const stanza = await promise
+    blue(stanza)
+  }
+
+  console.log('done')
 }
 
-function problemD () {
+async function problemD () {
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
    *
    * D. loggea todas las stanzas en el poema dos *en orden* asegurandote
@@ -136,21 +166,32 @@ function problemD () {
   filenames[randIdx] = 'wrong-file-name-' + (randIdx + 1) + '.txt';
 
   // callback version
-  async.eachSeries(filenames,
-    function (filename, eachDone) {
-      readFile(filename, function (err, stanza) {
-        console.log('-- D. callback version --');
-        if (err) return eachDone(err);
-        blue(stanza);
-        eachDone();
-      });
-    },
-    function (err) {
-      if (err) magenta(err);
-      console.log('-- D. callback version done --');
-    }
-  );
+  // async.eachSeries(filenames,
+  //   function (filename, eachDone) {
+  //     readFile(filename, function (err, stanza) {
+  //       console.log('-- D. callback version --');
+  //       if (err) return eachDone(err);
+  //       blue(stanza);
+  //       eachDone();
+  //     });
+  //   },
+  //   function (err) {
+  //     if (err) magenta(err);
+  //     console.log('-- D. callback version done --');
+  //   }
+  // );
 
   // AsyncAwait version
+  const promises = filenames.map((file) => promisifiedReadFile(file))
 
+  try {
+    for (const promise of promises) { 
+      const stanza = await promise
+      blue(stanza)
+    }
+  } catch (error) {
+    magenta(error)
+  } finally {
+    console.log('done')
+  }
 }
--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,16 @@
-function fizzBuzzGenerator(max) {
+function* fizzBuzzGenerator(max) {
   // Tu código acá:
+  let num = 1
 
+  // while / if ( (condición) ? (si se cumple) : (si no se cumple) )
+  while (max ? num <= max : true) { // Si tengo max y num es menor o igual a max, pero si no tengo max, es true y se genera un bucle infinito
+    if (num % 3 === 0 && num % 5 === 0) yield "Fizz Buzz"
+    else if (num % 3 === 0) yield "Fizz"
+    else if (num % 5 === 0) yield "Buzz"
+    else yield num
+    
+    num++
+  }
 }
 
 module.exports = fizzBuzzGenerator;
